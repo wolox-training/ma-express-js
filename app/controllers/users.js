ecdfbf499057ade2fbb8d5029c023f8fa334096e
Martin Acosta <martin.acosta@wolox.com.ar>
2020-12-21 14:40:28 -0300

    Email exists & password hashed in progress
    - Unique verified email
    - Password hashed in progress

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,45 +1,67 @@
-// const User = require('../models/')
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
-exports.register = async (req, res) => {
-  const { email, password, name, last_name } = req.body;
-  // Verifico que el request contenga todos los campos
-  const userValid = email && password && name && last_name ? true : false;
-  if (!userValid) res.send('Missing fields.');
+const User = require('../models/user.js');
+const errors = require('../middlewares/errors');
 
-  // Verifico que el mail sea válido
-  const validEmail = emailValidation(email);
-  if (!validEmail) res.send('Invalid email');
-
-  // Verifico que el email no exista previamente
-  // const emailExists = await Sequelize.findOne({ where: { email: email } });
-  // if (!emailExists === null) res.send('Email already exists.');
-  
-  // Verifico la longitud y composición de la contraseña
-  const validPass = passwordLength(password);
-  if (!validPass) res.send('Invalid password');
-  
-  res.send(userValid);
+// Verifica la validez de la contraseña (alfanumérica > 7)
+const passwordLength = password => {
+  const characters = /^[a-z0-9]+$/i;
+  const alfanumeric = characters.test(password);
+  const isValid = password.length > 7 && alfanumeric;
+  return isValid;
 };
 
-/**
- * Verifica la validez de la contraseña (alfanumérica > 7)
- * @param {String} password 
- */
-const passwordLength = password => {
-  var caracteres = /^[a-z0-9]+$/i;
-  var alfanum = caracteres.test(password);
-  const length = password.length > 7 ? true : false;
-  const valid = alfanum && length;
-  return valid;
-}
-
-/**
- * Verifica la validez y el dominio del email
- * @param {String} email 
- */
+// Verifica la validez y el dominio del email
 const emailValidation = email => {
-  const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  const emailValid = email.includes('@wolox.com.ar') ? true : false;
-  const valid = emailRegex && emailValid;
-  return valid;
-}
+  const emailRegex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+  const emailValid = email.includes('@wolox.com.ar');
+  const isValid = emailRegex && emailValid;
+  return isValid;
+};
+
+// Hashea el password mediante bcrypt
+const hashPassword = password => {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return Promise.reject(errors.hashError(err));
+    bcrypt.hash(password, salt, (error, hash) => {
+      if (error) return Promise.reject(errors.hashError(error));
+      return hash;
+    });
+    return false;
+  });
+};
+
+exports.register = async (req, res, next) => {
+  try {
+    const { email, password, name, last_name } = req.body;
+    // Verifico que el request contenga todos los campos
+    const userValid = email && password && name && last_name;
+    if (!userValid) res.send('Missing fields.');
+    const newUser = User.build({ email, password, name, last_name });
+
+    // Verifico que el mail sea válido
+    const validEmail = emailValidation(newUser.email);
+    if (!validEmail) res.send('Invalid email');
+
+    // Verifico que el email no exista previamente
+    const emailExists = await User.findOne({
+      where: {
+        email: {
+          [Op.eq]: newUser.email
+        }
+      }
+    });
+    if (emailExists) res.send('Email already exists.');
+
+    // Verifico la longitud y composición de la contraseña
+    const validPass = passwordLength(newUser.password);
+    if (!validPass) res.send('Invalid password');
+    const hashedPassword = hashPassword(newUser.password);
+
+    // res.send(userValid);
+    return res.send({ user: userValid, pass: hashedPassword });
+  } catch (error) {
+    return next(errors.notFound('User not found'));
+  }
+};
