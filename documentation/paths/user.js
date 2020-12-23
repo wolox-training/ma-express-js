a0396b2da2ce0bcc4d4e17918f2dc61676f36334
Martin Acosta <martin.acosta@wolox.com.ar>
2020-12-23 18:35:47 -0300

    Sign up docs
    - Path documentation
    - Schema documentation

--- documentation/paths/user.js
+++ documentation/paths/user.js
@@ -1,33 +1,5 @@
 module.exports = {
   '/users': {
-    get: {
-      tags: ['CRUD operations'],
-      description: 'Get users',
-      operationId: 'getUsers',
-      parameters: [
-        {
-          name: 'page',
-          in: 'query',
-          schema: {
-            type: 'integer',
-            default: 1
-          },
-          required: false
-        }
-      ],
-      responses: {
-        200: {
-          description: 'Users were obtained',
-          content: {
-            'application/json': {
-              schema: {
-                $ref: '#/components/schemas/Users'
-              }
-            }
-          }
-        }
-      }
-    },
     post: {
       tags: ['CRUD operations'],
       description: 'Create user',
@@ -44,19 +16,19 @@ module.exports = {
         required: true
       },
       responses: {
-        200: {
+        204: {
           description: 'New user was created'
         },
         400: {
-          description: 'Invalid parameters',
+          description: 'Email in use for another user',
           content: {
             'application/json': {
               schema: {
                 $ref: '#/components/schemas/Error'
               },
               example: {
-                message: 'User´s email already exists',
-                internal_code: 'invalid_parameters'
+                message: 'Email already in use.',
+                internal_code: 'unique_email_error'
               }
             }
           }
