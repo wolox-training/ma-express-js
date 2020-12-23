7c0a0c40c9a190b12332291db09e740f8bc65e56
Martin Acosta <martin.acosta@wolox.com.ar>
2020-12-23 13:45:59 -0300

    PR corrections
    - Implicit return in arrow functions within user service

--- app/services/users.js
+++ app/services/users.js
@@ -2,18 +2,14 @@ const { User } = require('../models');
 const logger = require('../logger');
 const errors = require('../errors');
 
-exports.emailExists = email => {
-  const existsUser = User.findOne({ where: { email } }).catch(error => {
+exports.emailExists = email =>
+  User.findOne({ where: { email } }).catch(error => {
     logger.error('Error while trying to get user by email', error.message);
     throw errors.databaseError(error.message);
   });
-  return existsUser;
-};
 
-exports.create = (email, password, name, lastName) => {
-  const createdUser = User.create({ email, password, name, lastName }).catch(error => {
+exports.create = (email, password, name, lastName) =>
+  User.create({ email, password, name, lastName }).catch(error => {
     logger.error('Error while trying to create an user', error.message);
     throw errors.databaseError(error.message);
   });
-  return createdUser;
-};
