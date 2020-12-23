f71bfbd40d15bdd7b7132744da3883a54fd9e8d7
Martin Acosta <martin.acosta@wolox.com.ar>
2020-12-23 13:03:27 -0300

    PR corrections
    - Separate error codes
    - DB error handling moved to user service file
    - Error case with no parameters received fixed

--- app/middlewares/errors.js
+++ app/middlewares/errors.js
@@ -5,7 +5,8 @@ const DEFAULT_STATUS_CODE = 500;
 const statusCodes = {
   [errors.DATABASE_ERROR]: 503,
   [errors.DEFAULT_ERROR]: 500,
-  [errors.CREATE_USER_ERROR]: 400
+  [errors.INVALID_PARAMS_ERROR]: 400,
+  [errors.UNIQUE_EMAIL_ERROR]: 400
 };
 
 exports.handle = (error, req, res, next) => {
--- app/services/users.js
+++ app/services/users.js
@@ -1,5 +1,19 @@
 const { User } = require('../models');
+const logger = require('../logger');
+const errors = require('../errors');
 
-exports.emailExists = email => User.findOne({ where: { email } });
+exports.emailExists = email => {
+  const existsUser = User.findOne({ where: { email } }).catch(error => {
+    logger.error('Error while trying to get user by email', error.message);
+    throw errors.databaseError(error.message);
+  });
+  return existsUser;
+};
 
-exports.create = (email, password, name, lastName) => User.create({ email, password, name, lastName });
+exports.create = (email, password, name, lastName) => {
+  const createdUser = User.create({ email, password, name, lastName }).catch(error => {
+    logger.error('Error while trying to create an user', error.message);
+    throw errors.databaseError(error.message);
+  });
+  return createdUser;
+};
