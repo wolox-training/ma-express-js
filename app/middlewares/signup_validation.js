6c5e4645399431310528c805eb787f99111a6e98
Martin Acosta <martin.acosta@wolox.com.ar>
2020-12-28 10:20:33 -0300

    Validation file created
    - Validation file created
    - Validation included

---
+++ app/middlewares/signup_validation.js
@@ -0,0 +1,6 @@
+// const logger = require('../logger');
+// const { checkSchema } = require('express-validator');
+
+exports.signUpValidation = (req, res, next) => {
+  next();
+};
--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,8 @@
 const { healthCheck } = require('./controllers/healthCheck');
 const { signUp } = require('./controllers/users');
+const { signUpValidation } = require('./middlewares/signup_validation');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.post('/users', signUp);
+  app.post('/users', [signUpValidation], signUp);
 };
