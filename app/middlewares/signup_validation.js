d3a2777f8a2165a32a868336c6c7cf8c2c47a06b
Martin Acosta <martin.acosta@wolox.com.ar>
2020-12-28 15:48:02 -0300

    Moved validations into middleware

--- app/middlewares/signup_validation.js
+++ app/middlewares/signup_validation.js
@@ -1,14 +1,13 @@
 const errors = require('../errors');
-const userService = require('../services/users');
 
-const passwordTest = password => {
+const passwordValidation = password => {
   const characters = /^[a-z0-9]+$/i;
   return password.length > 7 && characters.test(password);
 };
 
 const emailValidation = email => email.length > 13 && email.substring(email.length - 13) === '@wolox.com.ar';
 
-exports.signUpValidation = async (req, res, next) => {
+exports.signUpValidation = (req, res, next) => {
   try {
     const userValid = req.body.email && req.body.password && req.body.name && req.body.last_name;
     if (!userValid) throw errors.invalidParamsError('Something went wrong with params.');
@@ -16,15 +15,12 @@ exports.signUpValidation = async (req, res, next) => {
     req.body.email = req.body.email.trim();
     req.body.lastName = req.body.last_name;
 
-    const { email, password } = req.body;
-    const validEmail = emailValidation(email);
+    const validEmail = emailValidation(req.body.email);
     if (!validEmail) throw errors.invalidParamsError('Invalid email.');
 
-    const validPass = passwordTest(password);
+    const validPass = passwordValidation(req.body.password);
     if (!validPass) throw errors.invalidParamsError('Invalid password.');
 
-    const emailExists = await userService.emailExists(email);
-    if (emailExists) throw errors.uniqueEmailError('Email already in use.');
     return next();
   } catch (error) {
     return next(error);
