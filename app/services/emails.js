561449464b1081ab74a25edab1e47e6b13092695
Martin Acosta <martin.acosta@wolox.com.ar>
2021-01-27 15:10:24 -0300

    Congratulations email
    - Cron based scheduled function

--- app/services/emails.js
+++ app/services/emails.js
@@ -1,20 +1,31 @@
 const nodemailer = require('nodemailer');
 const { emailUser, emailPass } = require('../../config').common.session;
 
-exports.sendEmail = user => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-      user: emailUser,
-      pass: emailPass
-    }
-  });
-  const mailOptions = {
+const transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: emailUser,
+    pass: emailPass
+  }
+});
+
+exports.sendWelcomeEmail = user => {
+  const welcomeMailOptions = {
     from: 'Witter',
     to: user.email,
-    subject: '¡Bienvenido a Witter!',
-    text: `¡Bienvenido a Witter ${user.name} ${user.lastName}!`
+    subject: 'Welcome to Witter!',
+    text: `Welcome to Witter ${user.name} ${user.lastName}!`
   };
-  return transporter.sendMail(mailOptions);
+  return transporter.sendMail(welcomeMailOptions);
+};
+
+exports.sendCongratulationEmail = (user, stats) => {
+  const dailyMailOptions = {
+    from: 'Witter',
+    to: user.email,
+    subject: 'Congratulations!',
+    text: `Congratulations ${user.name} ${user.lastName}! You are the most Witterer of the day with ${stats.weets_quantity} Witters.`
+  };
+  return transporter.sendMail(dailyMailOptions);
 };
--- app/services/weets.js
+++ app/services/weets.js
@@ -1,7 +1,17 @@
-const { Weet } = require('../models');
+const { Op } = require('sequelize');
+const moment = require('moment');
+const { Weet, sequelize } = require('../models');
 const logger = require('../logger');
 const errors = require('../errors');
 const errorCatalog = require('../schemas/errors_catalog');
+
+const getDates = () => {
+  const today = moment().format();
+  const yesterday = moment()
+    .subtract(1, 'days')
+    .format();
+  return { yesterday, today };
+};
 
 exports.createWeet = weet =>
   Weet.create(weet).catch(error => {
@@ -22,4 +32,27 @@ exports.listWeets = async (page, limit) => {
     throw errors.databaseError(error.message);
   });
   return rawListWeets;
+};
+
+exports.wittererOfTheDay = async () => {
+  const { yesterday, today } = getDates();
+  const maxWitterer = await Weet.findOne({
+    attributes: ['creator_id', [sequelize.fn('COUNT', sequelize.col('id')), 'weets_quantity']],
+    group: ['creator_id'],
+    where: {
+      created_at: {
+        [Op.between]: [yesterday, today]
+      }
+    },
+    order: [[sequelize.literal('weets_quantity'), 'DESC']]
+  }).catch(error => {
+    logger.error('Error while trying to get the witterer of the day', error.message);
+    throw errors.databaseError(error.message);
+  });
+  if (!maxWitterer) return false;
+  return {
+    ...maxWitterer.dataValues,
+    yesterday,
+    today
+  };
 };
